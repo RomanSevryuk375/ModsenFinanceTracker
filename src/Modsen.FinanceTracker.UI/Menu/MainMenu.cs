75b15b02b1b60e69d252d484f4a6a13559d12eee
RomanSevryuk375 <sevryuk.roma@mail.ru>
2026-03-06 17:14:23 +0300

    feat(ui): implement MainMenu component

--- src/Modsen.FinanceTracker.UI/Menu/MainMenu.cs
+++ src/Modsen.FinanceTracker.UI/Menu/MainMenu.cs
@@ -1,6 +1,29 @@
-﻿namespace Modsen.FinanceTracker.UI.Menu;
+﻿using Spectre.Console;
+
+namespace Modsen.FinanceTracker.UI.Menu;
 
 public class MainMenu
 {
-    
+    public string ShowAndGetChoice()
+    {
+        AnsiConsole.Clear();
+        
+        var rule = new Rule("[yellow]Main Menu[/]");
+        rule.Justification = Justify.Left;
+        AnsiConsole.Write(rule);
+
+        AnsiConsole.WriteLine(); 
+
+        return AnsiConsole.Prompt(
+            new SelectionPrompt<string>()
+                .Title("Please select an [green]option[/]:")
+                .PageSize(10)
+                .AddChoices(new[] {
+                    "Add Transaction", 
+                    "View History", 
+                    "Delete Transaction", 
+                    "Check Balance", 
+                    "Exit"
+                }));
+    }
 }
