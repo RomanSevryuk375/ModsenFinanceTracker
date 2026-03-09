fecf282e2f3ac7ace33404720fb4210c61e38738
RomanSevryuk375 <sevryuk.roma@mail.ru>
2026-03-09 17:14:49 +0300

    feat(dal): implement generic repository interface and in-memory store

---
+++ src/Modsen.FinanceTracker.DAL/Repositories/TransactionRepository.cs
@@ -0,0 +1,21 @@
+﻿using Modsen.FinanceTracker.DAL.Context;
+using Modsen.FinanceTracker.Domain.Entities;
+using Modsen.FinanceTracker.Domain.Interfaces;
+
+namespace Modsen.FinanceTracker.DAL.Repositories;
+
+public class TransactionRepository : IRepository<Transaction>
+{
+    public IEnumerable<Transaction> GetAll() => InMemoryStorage.Transactions;
+    
+    public void Add(Transaction entity) => InMemoryStorage.Transactions.Add(entity);
+
+    public void Delete(Guid id)
+    {
+        var transaction = InMemoryStorage.Transactions.FirstOrDefault(t => t.Id == id);
+        if (transaction != null)
+        {
+            InMemoryStorage.Transactions.Remove(transaction);
+        }
+    }
+}
---
+++ src/Modsen.FinanceTracker.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace Modsen.FinanceTracker.Domain.Interfaces;
+
+public interface IRepository<T> where T : class
+{
+    IEnumerable<T> GetAll();
+    void Add(T entity);
+    void Delete(Guid id);
+}
