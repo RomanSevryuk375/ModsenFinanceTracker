add2eb34b42cae939076dca13b132ff270c3ccde
RomanSevryuk375 <sevryuk.roma@mail.ru>
2026-03-09 17:15:42 +0300

    feat(ui): create transaction list table view

---
+++ src/Modsen.FinanceTracker.UI/Views/TransactionListView.cs
@@ -0,0 +1,33 @@
+﻿using Modsen.FinanceTracker.Domain.Entities;
+using Spectre.Console;
+
+namespace Modsen.FinanceTracker.UI.Views;
+
+public class TransactionListView
+{
+    public void Render(IEnumerable<Transaction> transactions)
+    {
+        var table = new Table().Border(TableBorder.Rounded);
+        table.AddColumn("[yellow]Id[/]");
+        table.AddColumn("[yellow]Date[/]");
+        table.AddColumn("[yellow]Category[/]");
+        table.AddColumn("[yellow]Description[/]");
+        table.AddColumn("[yellow]Amount[/]");
+
+        foreach (var t in transactions)
+        {
+            string color = t is IncomeTransaction ? "green" : "red";
+            string sign = t is IncomeTransaction ? "+" : "-";
+            
+            table.AddRow(
+                t.Id.ToString(),
+                t.Date.ToShortDateString(),
+                "Mock",
+                t.Description,
+                $"[{color}]{sign}{t.Amount:C}[/]"
+            );
+        }
+
+        AnsiConsole.Write(table);
+    }
+}
