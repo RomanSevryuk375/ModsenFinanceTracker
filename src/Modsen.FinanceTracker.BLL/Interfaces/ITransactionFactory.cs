80fc59a976c53e0a6328f1f8a7c5da82de400252
RomanSevryuk375 <sevryuk.roma@mail.ru>
2026-03-16 17:58:19 +0300

    feat(bll): implement TransactionFactory

---
+++ src/Modsen.FinanceTracker.BLL/Interfaces/ITransactionFactory.cs
@@ -0,0 +1,13 @@
+using Modsen.FinanceTracker.Domain.Entities;
+using Modsen.FinanceTracker.Domain.Enums;
+
+namespace Modsen.FinanceTracker.BLL.Interfaces;
+
+public interface ITransactionFactory
+{
+    Transaction CreateTransaction(
+        TransactionType type, 
+        decimal amount, 
+        string description, 
+        Guid categoryId);
+}
